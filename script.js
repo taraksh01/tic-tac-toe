b0f43fddd0d9ae2676415983e76c0883a468c666
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-18 20:13:54 +0530

    Created a initializeGame funtion that initializes the game

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const cells = document.querySelectorAll(".cell");
+
 Gameboard = {
   gameboard: ["", "", "", "", "", "", "", "", ""],
 };
 
 function Player(name, marker) {
   return { name, marker };
+}
+
+function initializeGame() {
+  cells.forEach((cell) => {
+    cell.addEventListener("click", cellClicked);
+  });
 }
