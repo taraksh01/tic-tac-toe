2109b38d00c1ad1fae410dfa6e829e12db9305be
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-18 22:22:57 +0530

    Wraped all gameplay functions inside Game IIFE

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const cells = document.querySelectorAll(".cell");
-const start = document.querySelector(".start");
-const restart = document.querySelector(".restart");
-const status = document.querySelector(".game-status");
-let gameStarted = false;
-
 Gameboard = {
   gameboard: ["", "", "", "", "", "", "", "", ""],
   winningCondition: [
@@ -26,83 +20,91 @@ function Player(name, marker) {
   return { name, marker };
 }
 
-const player1 = Player("Player 1", "X");
-const player2 = Player("Player 2", "O");
+(function Game() {
+  const cells = document.querySelectorAll(".cell");
+  const start = document.querySelector(".start");
+  const restart = document.querySelector(".restart");
+  const status = document.querySelector(".game-status");
+  let gameStarted = false;
 
-let currentPlayer = player2;
+  const player1 = Player("Player 1", "X");
+  const player2 = Player("Player 2", "O");
 
-function initializeGame() {
-  cells.forEach((cell) => {
-    cell.addEventListener("click", cellClicked);
+  let currentPlayer = player2;
+
+  function initializeGame() {
+    cells.forEach((cell) => {
+      cell.addEventListener("click", cellClicked);
+    });
+    gameStarted = true;
+    status.textContent = `${currentPlayer.name}'s turn`;
+  }
+
+  function cellClicked() {
+    const index = this.id;
+    if (Gameboard.gameboard[index] !== "") {
+      status.textContent = "Cell is not empty choose another cell";
+    } else if (!gameStarted) {
+      return;
+    } else {
+      updateCell(this, index);
+      checkWinner();
+    }
+  }
+
+  function updateCell(cell, index) {
+    Gameboard.gameboard[index] = currentPlayer.marker;
+    cell.textContent = currentPlayer.marker;
+  }
+
+  function checkWinner() {
+    let gameWon = false;
+    for (let i = 0; i < 8; i++) {
+      const condition = Gameboard.winningCondition[i];
+      if (
+        Gameboard.gameboard[condition[0]] === "" ||
+        Gameboard.gameboard[condition[1]] === "" ||
+        Gameboard.gameboard[condition[2]] === ""
+      ) {
+        continue;
+      }
+      if (
+        Gameboard.gameboard[condition[0]] ===
+          Gameboard.gameboard[condition[1]] &&
+        Gameboard.gameboard[condition[1]] === Gameboard.gameboard[condition[2]]
+      ) {
+        gameWon = true;
+        break;
+      }
+    }
+
+    if (gameWon) {
+      status.textContent = `${currentPlayer.name} Won the game!!!!`;
+      gameStarted = false;
+    } else if (!Gameboard.gameboard.includes("")) {
+      status.textContent = "It's a draw";
+    } else {
+      changePlayer();
+    }
+  }
+
+  function changePlayer() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    status.textContent = `${currentPlayer.name}'s turn`;
+  }
+
+  start.addEventListener("click", () => {
+    status.textContent = `Your marker is ${player1.marker}`;
+    initializeGame();
   });
-  gameStarted = true;
-  status.textContent = `${currentPlayer.name}'s turn`;
-}
 
-function cellClicked() {
-  const index = this.id;
-  console.log(index);
-  if (Gameboard.gameboard[index] !== "") {
-    status.textContent = "Cell is not empty choose another cell";
-  } else if (!gameStarted) {
-    return;
-  } else {
-    updateCell(this, index);
-    checkWinner();
-  }
-}
-
-function updateCell(cell, index) {
-  Gameboard.gameboard[index] = currentPlayer.marker;
-  cell.textContent = currentPlayer.marker;
-}
-
-function checkWinner() {
-  let gameWon = false;
-  for (let i = 0; i < 8; i++) {
-    const condition = Gameboard.winningCondition[i];
-    if (
-      Gameboard.gameboard[condition[0]] === "" ||
-      Gameboard.gameboard[condition[1]] === "" ||
-      Gameboard.gameboard[condition[2]] === ""
-    ) {
-      continue;
-    }
-    if (
-      Gameboard.gameboard[condition[0]] === Gameboard.gameboard[condition[1]] &&
-      Gameboard.gameboard[condition[1]] === Gameboard.gameboard[condition[2]]
-    ) {
-      gameWon = true;
-      break;
-    }
+  function restartGame() {
+    currentPlayer = player2;
+    Gameboard.setGameBoard(["", "", "", "", "", "", "", "", ""]);
+    status.textContent = `${currentPlayer.name}'s turn`;
+    cells.forEach((cell) => (cell.textContent = ""));
+    gameStarted = true;
   }
 
-  if (gameWon) {
-    status.textContent = `${currentPlayer.name} Won the game!!!!`;
-    gameStarted = false;
-  } else if (!Gameboard.gameboard.includes("")) {
-    status.textContent = "It's a draw";
-  } else {
-    changePlayer();
-  }
-}
-
-function changePlayer() {
-  currentPlayer = currentPlayer === player1 ? player2 : player1;
-  status.textContent = `${currentPlayer.name}'s turn`;
-}
-
-start.addEventListener("click", () => {
-  status.textContent = `Your marker is ${player1.marker}`;
-  initializeGame();
-});
-
-function restartGame() {
-  currentPlayer = player2;
-  Gameboard.setGameBoard(["", "", "", "", "", "", "", "", ""]);
-  status.textContent = `${currentPlayer.name}'s turn`;
-  cells.forEach((cell) => (cell.textContent = ""));
-  gameStarted = true;
-}
-
-restart.addEventListener("click", restartGame);
+  restart.addEventListener("click", restartGame);
+})();
