c576db7d4cf51f6afbd07888fadafe4c621e2c71
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-18 20:40:08 +0530

    Added winningCondition inside Gameboard object

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const cells = document.querySelectorAll(".cell");
 
 Gameboard = {
   gameboard: ["", "", "", "", "", "", "", "", ""],
+  winningCondition: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
 };
 
 function Player(name, marker) {
